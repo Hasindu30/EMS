5ecb5795173352fb8a99f87706a4221b5e19e3a4
Hasindu Chandeepa <chandeepahasindu@gmail.com>
2025-04-10 23:22:24 +0530

    create pagesetup

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"], // Include jsx/tsx if using React/TypeScript
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
